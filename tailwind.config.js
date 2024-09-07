35e63232ee88ae81535fc986f3a72f1706502a21
Vivek <vivekchaudhary45500@gmail.com>
2024-09-07 23:56:56 +0530

    Login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily:
+      {
+        inter:['Inter']
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
